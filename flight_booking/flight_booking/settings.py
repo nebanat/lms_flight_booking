2a865511a8d7c65d675c46bc738bf17d76f93b68
Aaron Biliyok <abiliyok@gmail.com>
2019-04-17 19:43:02 +0100

    include procfile for deployment

--- flight_booking/flight_booking/settings.py
+++ flight_booking/flight_booking/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from celery.schedules import crontab
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
