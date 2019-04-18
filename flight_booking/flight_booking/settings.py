892c63941d93ac0ecdbeee7bff980606ae541eab
Aaron Biliyok <abiliyok@gmail.com>
2019-04-18 08:58:33 +0100

    change debug status

--- flight_booking/flight_booking/settings.py
+++ flight_booking/flight_booking/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8+3g-6b)e^f+4mlmdn13+tkwk#ee^nf3g6!@+jd7-pmwvs5b*d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
